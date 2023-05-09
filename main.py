08a6093ee9fbfeff3795ec045bd97e6a7bde3b82
jaredkirby <jpeterkirby@gmail.com>
2023-05-09 06:34:07 -0700

    Updates

--- main.py
+++ main.py
@@ -1,61 +1,216 @@
-import langchain
-import openai
-import streamlit as st
+from langchain.chains import LLMChain
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    AIMessagePromptTemplate
+)
+from utils import (
+    chat_35_0,
+    chat_35_07,
+    chat_35_05,
+    chat_35_1,
+    chat_4_07,
+    chat_4_05,
+    chat_4_0,
+    chat_4_2,
+    chat_4_1,
+    chat_4_15
+)
 
-# Is it better if the model has access to all generated answers?
-# Or should it only have access to one at a time?
 
+def generate_multiple_initial_answers(question, n):
+    answer_list = []
+
+    answer_sys_prompt = '''
+You are AnswerPilot, a large language model trained by OpenAI and prompt
+engineered by Jared Kirby.
+Your task is to provide detailed, step-by-step answers to questions.
+Use reliable sources, do not fabricate information, and cite your sources when possible.
+If unsure of an answer, express that you do not know.
+Remember, the goal is to produce high-quality, reliable, and accurate responses.
 '''
-Step 1: Inital "Question" + "Answer: Let's work this out in a step by step way to be 
-    sure we have the right answer."
+    answer_sys_message_prompt = SystemMessagePromptTemplate.from_template(
+        answer_sys_prompt)
 
-Step 2: Send the prompt from step 1 to GPT-4 3x to get 3 different answers. Test with 
-    various temp and top_p values.
+    answer_human_prompt = PromptTemplate(
+        template='''
+Question: Can you provide a step-by-step method to solve the following problem?
+{question}
+    ''', input_variables=["question"])
+    answer_human_message_prompt = HumanMessagePromptTemplate(
+        prompt=answer_human_prompt)
 
-Step 3: Analysis of the answers.
-    Prompt: 
-    "You are a logic researcher tasked with conducting the following for each 
-    answer option provided to you:
-        - List the flaws and faulty logic for each answer option.
-        - List the strengths and sound logic for each answer option.
-    
-    Let's work through each answer option in a step by step way to insure all 
-    flaws and strengths are identified.
-    
-    Format your response as follows:
-        Original Question: "Original Question"
-        - Answer Option 1: "Answer Option 1"
-            - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
-            - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
-        - Answer Option 2: "Answer Option 2"
-            - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
-            - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
-        - Answer Option 3: "Answer Option 3"
-            - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
-            - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
-            
-Step 4: Prompt: 
-    "You are a resolver tasked with the following:
-        - Analyze the question and answer data given to you. 
-            Take into account the following:
-                - The users origianl question
-                - The answer option provided
-                - The identified flaws and strengths of the answer option
-        - Resolve the answer by providing a new answer that addresses the flaws and 
-            enhances the strengths for each answer option.
-    
-    Let's work through this in a step by step way to insure we provide the best 
-        possible answers given the information provided.
-    
-    Format your response as follows:
-        Original Question: "Original Question"
-        - Updated Answer: "Updated Answer"
+    answer_ai_prompt = '''
+Sure, let's break down the problem and work through it step by step to arrive
+at the correct solution.
 
-Step 5: Prompt: 
-    "You are an answer selector tasked with the following:
-        - Analyze the original question and the list of answers given to you.
-        - Select the best answer given the original question and the list of answers.
-        - Respond with the best answer.
-    
-    Let's work through this in a step by step way to insure we have the best answer.
+Here are the steps: ...
 '''
+    answer_ai_message_prompt = AIMessagePromptTemplate.from_template(
+        answer_ai_prompt)
+
+    answer_prompt = ChatPromptTemplate.from_messages(
+        [answer_sys_message_prompt, answer_human_message_prompt,
+         answer_ai_message_prompt])
+    answer_chain = LLMChain(
+        verbose=True, llm=chat_35_1, prompt=answer_prompt)
+
+    for i in range(n):
+        answer = answer_chain.run({'question': question})
+        answer = answer.split("\n")
+        answer_list.append(answer)
+
+    return answer_list
+
+
+def analyze_answers(question, answer_list):
+    analyze_sys_prompt = '''
+You are AnalyzePilot, a large language model trained by OpenAI and prompt
+engineered by Jared Kirby. Your task is to analyze the answers provided, 
+identifying the flaws and strengths in logic for each answer option. 
+Remember to use a systematic, step-by-step approach to ensure all aspects are 
+considered. Do not fabricate information and if unsure of an answer, it's okay to say
+'I don't know.' Present your response in a structured format, as outlined below.
+'''
+    analyze_sys_message_prompt = SystemMessagePromptTemplate.from_template(
+        analyze_sys_prompt)
+
+    analyze_human_prompt = PromptTemplate(
+        template='''
+As an AI trained on a broad range of information, please analyze the following answers 
+for their logic, strengths, and weaknesses:
+Original Question: {question}
+
+Answer List:
+{answer_list}
+
+Format your response as follows:
+Original Question: "Original Question"
+- Answer Option 1: "Answer Option 1"
+    - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
+    - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
+- Answer Option 2: "Answer Option 2"
+    - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
+    - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
+- Answer Option 3: "Answer Option 3"
+    - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
+    - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
+- ...
+    ''', input_variables=["question", "answer_list"])
+    analyze_human_message_prompt = HumanMessagePromptTemplate(
+        prompt=analyze_human_prompt)
+
+    analyze_prompt = ChatPromptTemplate.from_messages(
+        [analyze_sys_message_prompt, analyze_human_message_prompt])
+    analyze_chain = LLMChain(
+        verbose=True, llm=chat_4_0, prompt=analyze_prompt)
+    analysis = analyze_chain.run(
+        {'question': question, 'answer_list': answer_list})
+    analysis = analysis.split("\n")
+    return analysis
+
+
+def resolve_answers(question, analysis):
+    resolve_sys_prompt = '''
+You are ResolvePilot, a large language model trained by OpenAI and prompt
+engineered by Jared Kirby. Your task is to analyze the question and answer 
+data provided to you, and resolve each answer by addressing the flaws and 
+enhancing the strengths identified. Remember to work systematically and 
+step by step, using reliable information. If unsure of an answer, it's 
+okay to say 'I don't know.' The response should be formatted as outlined below.
+'''
+    resolve_sys_message_prompt = SystemMessagePromptTemplate.from_template(
+        resolve_sys_prompt)
+
+    resolve_human_prompt = PromptTemplate(
+        template='''
+As an AI trained on a broad range of information, please help me improve the 
+following answers by addressing the flaws and enhancing the strengths, based 
+on the analysis provided:
+Original Question: {question}
+
+Answer List:
+{analysis}
+
+Format your response as follows:
+Original Question: "Original Question"
+    - Updated Answer 1: "Updated Answer"
+    - Updated Answer 2: "Updated Answer"
+    - Updated Answer 3: "Updated Answer"
+    - ...
+    ''', input_variables=["question", "analysis"])
+    resolve_human_message_prompt = HumanMessagePromptTemplate(
+        prompt=resolve_human_prompt)
+
+    resolve_prompt = ChatPromptTemplate.from_messages(
+        [resolve_sys_message_prompt, resolve_human_message_prompt])
+    resolve_chain = LLMChain(
+        verbose=True, llm=chat_4_0, prompt=resolve_prompt)
+    resolved_answers = resolve_chain.run(
+        {'question': question, 'analysis': analysis})
+    return resolved_answers
+
+
+def select_answer(question, resolved_answers):
+    select_sys_prompt = '''
+You are SelectPilot, a large language model trained by OpenAI and prompt
+engineered by Jared Kirby. Your task is to analyze the original question 
+and the list of answers provided, and then select the best answer based 
+on the information given. Remember to work systematically and step by step, 
+using reliable information. If unsure of an answer, it's okay to say 'I don't know.' 
+The response should be formatted as outlined below.
+'''
+    select_sys_message_prompt = SystemMessagePromptTemplate.from_template(
+        select_sys_prompt)
+
+    select_human_prompt = PromptTemplate(
+        template='''
+As an AI trained on a broad range of information, please help me select the best 
+answer for the following question and list of answers:
+Original Question: {question}
+
+Answer List:
+{resolved_answers}
+
+Format your response as follows:
+Original Question: "Original Question"
+    - Selected Answer: "Selected Answer"
+    ''', input_variables=["question", "resolved_answers"])
+    select_human_message_prompt = HumanMessagePromptTemplate(
+        prompt=select_human_prompt)
+
+    select_prompt = ChatPromptTemplate.from_messages(
+        [select_sys_message_prompt, select_human_message_prompt])
+    select_chain = LLMChain(
+        verbose=True, llm=chat_4_0, prompt=select_prompt)
+    selected_answer = select_chain.run({'question': question,
+                                        'resolved_answers': resolved_answers})
+    return selected_answer
+
+
+def main():
+    # Ask the user for the question and iteration number 'n'
+    question = input("What is your question? ")
+    n = int(input("How many answers do you want? "))
+
+    # Generate 'n' answers to the question
+    answer_list = generate_multiple_initial_answers(question, n)
+    print("Generated answers:", answer_list)
+
+    # Analyze all answers
+    analysis = analyze_answers(question, answer_list)
+    print("Analysis of answers:", analysis)
+
+    # Resolve answers using analysis from analyze_answers()
+    resolved_answers = resolve_answers(question, analysis)
+    print("Resolved answers:", resolved_answers)
+
+    # Select the best answer from the resolved answers
+    selected_answer = select_answer(question, resolved_answers)
+    print("Selected best answer:", selected_answer)
+
+
+if __name__ == "__main__":
+    main()
---
+++ notes.py
@@ -0,0 +1,58 @@
+
+# Is it better if the model has access to all generated answers?
+# Or should it only have access to one at a time?
+
+'''
+Step 1: Inital "Question" + "Answer: Let's work this out in a step by step way to be 
+    sure we have the right answer."
+
+Step 2: Send the prompt from step 1 to GPT-4 3x to get 3 different answers. Test with 
+    various temp and top_p values.
+
+Step 3: Analysis of the answers.
+    Prompt: 
+    "You are a logic researcher tasked with conducting the following for each 
+    answer option provided to you:
+        - List the flaws and faulty logic for each answer option.
+        - List the strengths and sound logic for each answer option.
+    
+    Let's work through each answer option in a step by step way to insure all 
+    flaws and strengths are identified.
+    
+    Format your response as follows:
+        Original Question: "Original Question"
+        - Answer Option 1: "Answer Option 1"
+            - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
+            - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
+        - Answer Option 2: "Answer Option 2"
+            - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
+            - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
+        - Answer Option 3: "Answer Option 3"
+            - Identified Flaws: "Flaw 1", "Flaw 2", "Flaw 3, etc."
+            - Identified Strengths: "Strength 1", "Strength 2", "Strength 3, etc."
+            
+Step 4: Prompt: 
+    "You are a resolver tasked with the following:
+        - Analyze the question and answer data given to you. 
+            Take into account the following:
+                - The users origianl question
+                - The answer option provided
+                - The identified flaws and strengths of the answer option
+        - Resolve the answer by providing a new answer that addresses the flaws and 
+            enhances the strengths for each answer option.
+    
+    Let's work through this in a step by step way to insure we provide the best 
+        possible answers given the information provided.
+    
+    Format your response as follows:
+        Original Question: "Original Question"
+        - Updated Answer: "Updated Answer"
+
+Step 5: Prompt: 
+    "You are an answer selector tasked with the following:
+        - Analyze the original question and the list of answers given to you.
+        - Select the best answer given the original question and the list of answers.
+        - Respond with the best answer.
+    
+    Let's work through this in a step by step way to insure we have the best answer.
+'''
