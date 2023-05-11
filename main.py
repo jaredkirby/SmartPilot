a8b2f89b1d323a06b438ee514d8fadfb2216605a
Jared Kirby <jpeterkirby@gmail.com>
2023-05-11 12:52:07 -0700

    Fix: ChatOpenAI Import / Update: model_name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from langchain.chains import ChatOpenAI
+from secret import OPENAI_API_KEY
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import (
     ChatPromptTemplate,
@@ -8,22 +9,22 @@ from langchain.prompts import (
     AIMessagePromptTemplate
 )
 
-openai_api_key = 'YOUR_OPENAI_API_KEY_HERE'
+openai_api_key = OPENAI_API_KEY
 chat_35_0 = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
 chat_35_07 = ChatOpenAI(temperature=0.7, openai_api_key=openai_api_key)
 chat_35_05 = ChatOpenAI(temperature=0.5, openai_api_key=openai_api_key)
 chat_35_1 = ChatOpenAI(temperature=1, openai_api_key=openai_api_key)
-chat_4_07 = ChatOpenAI(model="gpt-4", temperature=0.7,
+chat_4_07 = ChatOpenAI(model_name="gpt-4", temperature=0.7,
                        openai_api_key=openai_api_key, request_timeout=240)
-chat_4_05 = ChatOpenAI(model="gpt-4", temperature=0.5,
+chat_4_05 = ChatOpenAI(model_name="gpt-4", temperature=0.5,
                        openai_api_key=openai_api_key, request_timeout=240)
-chat_4_0 = ChatOpenAI(model="gpt-4", temperature=0,
+chat_4_0 = ChatOpenAI(model_name="gpt-4", temperature=0,
                       openai_api_key=openai_api_key, request_timeout=240)
-chat_4_2 = ChatOpenAI(model="gpt-4", temperature=2,
+chat_4_2 = ChatOpenAI(model_name="gpt-4", temperature=2,
                       openai_api_key=openai_api_key, request_timeout=240)
-chat_4_1 = ChatOpenAI(model="gpt-4", temperature=1,
+chat_4_1 = ChatOpenAI(model_name="gpt-4", temperature=1,
                       openai_api_key=openai_api_key, request_timeout=240)
-chat_4_15 = ChatOpenAI(model="gpt-4", temperature=1.5,
+chat_4_15 = ChatOpenAI(model_name="gpt-4", temperature=1.5,
                        openai_api_key=openai_api_key, request_timeout=240)
 
 
